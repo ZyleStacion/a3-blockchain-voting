687dbe718213fa629c6fa5ff93c6bbaa9584b00b
ZyleStacion <s4064846@rmit.edu.vn>
2025-08-29 23:32:17 +0700

    add some content to home

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,31 @@ import './App.css'
 
 function App() {
   return (
-    <div>
-      <h1>DAO Voting System</h1>
-      <p>Welcome to our blockchain voting dApp 🚀</p>
+    <>
+    {/* Logo, title and description */}
+    <div class="home-view">
+      <div class="content-main">
+        <div class="logo">
+          <p class="emoji-main" alt="Trophy Emoji">🏆</p>
+        </div>
+        <div class="home-text">
+          <h1>BlockAid</h1>
+          <p>Welcome to our blockchain voting app🚀</p>
+        </div>
+      </div>
     </div>
+
+    {/* Call to action: ask users to donate */}
+    <div class="call-to-action">
+      <div class="cta-text">
+        <h2>Donate Now</h2>
+        <p>Disclaimer</p>
+      </div>
+      <div class="cta-button">
+        <button>Cast my vote</button>
+      </div>
+    </div>
+    </>
   )
 }
 
